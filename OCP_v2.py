15554c62489c1e5308ba69481a050ca6d5ba0e39
Mhammad Sayegh <mhammadsayegh@gmail.com>
2020-11-30 20:36:35 +0100

    added second code

--- OCP_v2.py
+++ OCP_v2.py
@@ -1,6 +1,7 @@
-"""
 
-Created on Thu Nov 26
+
+"""
+Created on Thu Nov 29
 
 Ninja Robot Thesis
 
@@ -19,6 +20,11 @@ Ninja Robot Thesis
  
 4) Should generate more case scnearios to make sure this
    works in all cases
+   
+   recieved feedback = here I add two path parameters instead of one
+   
+   #### note: the code still sometimes doesnt work =  constraints violated on end goal, or control v
+                                                   =  when I change the weights = is this normal ?
 
 
 @author: Mohamad Sayegh
@@ -92,6 +98,7 @@ global_path = interpolate.splev(u, Bspline_obj)
 #                           Creating the Bubbles                             #
 #----------------------------------------------------------------------------#
 
+
 #using functions 
 feasiblebubbles_x, feasiblebubbles_y,shifted_feasiblebubbles_x,\
 shifted_feasiblebubbles_y,midpoints_x, midpoints_y, radii,\
@@ -104,13 +111,17 @@ plotting(initial_pos_x, end_goal_x, global_path, occupied_positions_x, occupied_
              shifted_feasiblebubbles_x, shifted_feasiblebubbles_y, midpoints_x, midpoints_y, radii,\
              shifted_midpoints_x, shifted_midpoints_y, shifted_radii)
 
+
 #----------------------------------------------------------------------------#
 #                  Creating the new shifted path                             #
 #----------------------------------------------------------------------------#
 
 #this path will be used only for the obstacle avoidance = creating by the new midpoints
 
-shifted_Bspline_obj, u = interpolate.splprep([shifted_midpoints_x,shifted_midpoints_y], u = None, s = 0)
+#change s here to control smoothness of path = could affect the ocp
+shifted_Bspline_obj, u = interpolate.splprep([shifted_midpoints_x,shifted_midpoints_y], u = None, s = 1) 
+
+
 u = np.linspace(0,1,100)
 shifted_global_path = interpolate.splev(u, shifted_Bspline_obj)
 
@@ -145,15 +156,19 @@ theta   =  ocp.state()
 v       =  ocp.control()
 w       =  ocp.control()
 
-#path parameter s
-s       =  ocp.state()
-sdot    =  ocp.control()
+#path parameters s1 and s2
+s1       =  ocp.state()
+sdot1    =  ocp.control()
+
+s2       =  ocp.state()
+sdot2    =  ocp.control()
 
 #ODEs
 ocp.set_der(x       ,        v*cos(theta))
 ocp.set_der(y       ,        v*sin(theta))
 ocp.set_der(theta   ,        w)
-ocp.set_der(s       ,        sdot)
+ocp.set_der(s1      ,        sdot1)
+ocp.set_der(s2      ,        sdot2)
 
 
 
@@ -167,20 +182,22 @@ ocp.set_initial(theta,  0.0)
 ocp.subject_to(ocp.at_t0(x)      == initial_pos_x)
 ocp.subject_to(ocp.at_t0(y)      == initial_pos_y)
 ocp.subject_to(ocp.at_t0(theta)  == 0.0)
-ocp.subject_to(ocp.at_t0(s)      == 0.0)     
+ocp.subject_to(ocp.at_t0(s1)     == 0.0)     
+ocp.subject_to(ocp.at_t0(s2)     == 0.0)    
 
 #Constraints at tf
 ocp.subject_to(ocp.at_tf(x) == end_goal_x)
 ocp.subject_to(ocp.at_tf(y) == end_goal_y)
 ocp.subject_to(ocp.at_tf(theta) == 0.0)
-ocp.subject_to(ocp.at_tf(s) == 1)
+ocp.subject_to(ocp.at_tf(s1) == 1)              #this constraint on s is sometimes violated. why ?
+ocp.subject_to(ocp.at_tf(s2) == 1)
 
 
 #constraints on controls 
-ocp.subject_to(  0    <= ( v  <= 1   ))
-ocp.subject_to( -pi   <= ( w  <= pi  ))
-ocp.subject_to( sdot  >=   0)              #path "increasing"
-
+ocp.subject_to(  0      <=  ( v  <= 1   ))
+ocp.subject_to( -pi     <=  ( w  <= pi  ))
+ocp.subject_to(  sdot1  >=  0)                      #path "increasing"
+ocp.subject_to(  sdot2  >=  0)   
 
 
 #-----------  use the midpoints and radius to create interpolated tunnels
@@ -188,20 +205,20 @@ ocp.subject_to( sdot  >=   0)              #path "increasing"
 
 #------------ Obscatles avoidance tunnel
 
-bubbles_radii   =  shifted_radii
-bubbles_x       =  shifted_midpoints_x
-bubbles_y       =  shifted_midpoints_y
-tlength         =  len(bubbles_x)
-tunnel_s        =  np.linspace(0,1,tlength) 
+bubbles_radii     =  shifted_radii
+bubbles_x         =  shifted_midpoints_x
+bubbles_y         =  shifted_midpoints_y
+tlength1          =  len(bubbles_x)
+tunnel_s1         =  np.linspace(0,1,tlength1) 
 
 
-ocp.set_initial(s, np.linspace(tunnel_s[0],tunnel_s[-7], N))  #dont take the last points in tunnel_s so the path wont go back to zero
-ocp.set_initial(sdot, (tunnel_s[-2]-tunnel_s[0])/tlength)
+ocp.set_initial(s1, np.linspace(tunnel_s1[0],tunnel_s1[-9], N))  #dont take the last points in tunnel_s so the path wont go back to zero
+ocp.set_initial(sdot1, (tunnel_s1[-2]-tunnel_s1[0])/tlength1)
 
 
-obs_spline_x = interpolant('x','bspline',[tunnel_s],bubbles_x,{"algorithm": "smooth_linear","smooth_linear_frac":0.1})
-obs_spline_y = interpolant('y','bspline',[tunnel_s],bubbles_y,{"algorithm": "smooth_linear","smooth_linear_frac":0.1})
-obs_spline_r = interpolant('r','bspline',[tunnel_s],bubbles_radii,{"algorithm": "smooth_linear","smooth_linear_frac":0.1})
+obs_spline_x = interpolant('x','bspline',[tunnel_s1],bubbles_x,{"algorithm": "smooth_linear","smooth_linear_frac":0.1})
+obs_spline_y = interpolant('y','bspline',[tunnel_s1],bubbles_y,{"algorithm": "smooth_linear","smooth_linear_frac":0.1})
+obs_spline_r = interpolant('r','bspline',[tunnel_s1],bubbles_radii,{"algorithm": "smooth_linear","smooth_linear_frac":0.1})
 
 
 #------------ Path Tunnel avoidance 
@@ -209,13 +226,17 @@ obs_spline_r = interpolant('r','bspline',[tunnel_s],bubbles_radii,{"algorithm": 
 #re-evaluate the path only in same length as bubbles data
 
 
-global_path     =  interpolate.splev(tunnel_s,Bspline_obj)
-path_x          =  global_path[0]
-path_y          =  global_path[1]
+path_x         =  global_path[0]
+path_y         =  global_path[1]
+tlength2       =  len(path_x)
+tunnel_s2      =  np.linspace(0,1,tlength2) 
 
 
-path_spline_x = interpolant('x','bspline',[tunnel_s],path_x,{"algorithm": "smooth_linear","smooth_linear_frac":0.49})  #low fraction ?
-path_spline_y = interpolant('y','bspline',[tunnel_s],path_y,{"algorithm": "smooth_linear","smooth_linear_frac":0.49})
+ocp.set_initial(s2, np.linspace(tunnel_s2[0],tunnel_s2[-9], N))  #dont take the last points in tunnel_s so the path wont go back to zero
+ocp.set_initial(sdot1, (tunnel_s2[-2]-tunnel_s2[0])/tlength2)
+
+path_spline_x = interpolant('x','bspline',[tunnel_s2],path_x,{"algorithm": "smooth_linear","smooth_linear_frac":0.49})  #low fraction ?
+path_spline_y = interpolant('y','bspline',[tunnel_s2],path_y,{"algorithm": "smooth_linear","smooth_linear_frac":0.49})
 
 #---------------- Slack variables for soft constraints 
 
@@ -226,20 +247,20 @@ ocp.subject_to(slack_obs      >= 0)
 ocp.subject_to(slack_next_obs >= 0)
 
 #stay in bubbles as much as possible
-ocp.subject_to( (  ( x - obs_spline_x(s) )**2 + ( y-obs_spline_y(s))**2  - obs_spline_r(s)**2 ) <= slack_obs )
+ocp.subject_to( (  ( x - obs_spline_x(s1) )**2 + ( y-obs_spline_y(s1))**2  - obs_spline_r(s1)**2 ) <= slack_obs )
 
 #stay in next bubbles as much as possible
-ocp.subject_to( (  ( x - obs_spline_x(ocp.next(s)) )**2 + ( y-obs_spline_y(ocp.next(s)))**2  - obs_spline_r(ocp.next(s))**2 ) <= slack_next_obs )
+ocp.subject_to( (  ( x - obs_spline_x(ocp.next(s1)) )**2 + ( y-obs_spline_y(ocp.next(s1)))**2  - obs_spline_r(ocp.next(s1))**2 ) <= slack_next_obs )
  
 
 # ------------- Objective function 
 
 #path following
-ocp.add_objective(3.5*ocp.integral((x - path_spline_x(s))**2 + (y-path_spline_y(s))**2))
+ocp.add_objective(2*ocp.integral((x - path_spline_x(s2))**2 + (y-path_spline_y(s2))**2))
 
-ocp.add_objective(2*slack_obs)
+ocp.add_objective(3*slack_obs)
 
-ocp.add_objective(2*slack_next_obs)
+ocp.add_objective(3*slack_next_obs)
 
 
 # ------------- Solution method
@@ -272,14 +293,14 @@ plt.xlabel('x [m]')
 plt.ylabel('y [m]')
 plt.xlim([xlim_min,xlim_max])
 plt.ylim([ylim_min,ylim_max])
-ts, s_obs = sol.sample(s, grid='integrator',refine = 100)
+ts, s_obs = sol.sample(s1, grid='integrator',refine = 200)
 ts = np.linspace(0,2*np.pi,1000)
 xspline_obs = np.array(obs_spline_x(s_obs))
 yspline_obs = np.array(obs_spline_y(s_obs))
 rspline_obs = np.array(obs_spline_r(s_obs))
 for i in range(s_obs.shape[0]): plt.plot(xspline_obs[i]+rspline_obs[i]*cos(ts),yspline_obs[i]+rspline_obs[i]*sin(ts),'r-',markersize = 0.5)
 
-ts, s_path = sol.sample(s, grid='integrator',refine = 100)
+ts, s_path = sol.sample(s2, grid='integrator',refine = 200)
 ts = np.linspace(0,2*np.pi,1000)
 xspline_path = np.array(path_spline_x(s_path))
 yspline_path = np.array(path_spline_y(s_path))
@@ -309,30 +330,30 @@ plt.xlim([xlim_min,xlim_max])
 plt.ylim([ylim_min,ylim_max])
 
 
-plt.figure()
-plt.plot(global_path[0], global_path[1], 'g--')
-plt.plot(occupied_positions_x, occupied_positions_y, 'bo', markersize = 1)
-plt.plot(xsol, ysol, 'rx', markersize = 5)
-plt.legend(['original path', 'obstacles','solution'])
-plt.title('Solution compared to initial global path')
-plt.xlim([2.9,5])
-plt.ylim([-0.2,3])
+# plt.figure()
+# plt.plot(global_path[0], global_path[1], 'g--')
+# plt.plot(occupied_positions_x, occupied_positions_y, 'bo', markersize = 1)
+# plt.plot(xsol, ysol, 'rx', markersize = 5)
+# plt.legend(['original path', 'obstacles','solution'])
+# plt.title('Solution compared to initial global path')
+# plt.xlim([2.9,5])
+# plt.ylim([-0.2,3])
 
-#plot velocities to verify that velocities are high (close to the limit)
-tsol, vsol = sol.sample(v, grid='integrator')
-tsol, wsol = sol.sample(w, grid='integrator')
+# #plot velocities to verify that velocities are high (close to the limit)
+# tsol, vsol = sol.sample(v, grid='integrator')
+# tsol, wsol = sol.sample(w, grid='integrator')
 
-plt.figure()
-plt.plot(tsol,vsol)
-plt.title('Linear Velocity of the solution')
-plt.xlabel('Solution Time [s]')
-plt.ylabel('Linear velocity [m/s]')
+# plt.figure()
+# plt.plot(tsol,vsol)
+# plt.title('Linear Velocity of the solution')
+# plt.xlabel('Solution Time [s]')
+# plt.ylabel('Linear velocity [m/s]')
 
-plt.figure()
-plt.plot(tsol,wsol)
-plt.title('Angular Velocity of the solution')
-plt.xlabel('Solution Time [s]')
-plt.ylabel('Angular velocity [rad/s]')
+# plt.figure()
+# plt.plot(tsol,wsol)
+# plt.title('Angular Velocity of the solution')
+# plt.xlabel('Solution Time [s]')
+# plt.ylabel('Angular velocity [rad/s]')
 
 
 
